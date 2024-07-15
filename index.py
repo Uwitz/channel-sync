8745afcc8398b2d93990c10a7178c80177a2bab1
Snyco <hello@snyco.dev>
2024-07-15 15:30:45 +0800

    Added bot's core file.

---
+++ index.py
@@ -0,0 +1,48 @@
+import os
+import traceback
+
+from typing import Any, Coroutine
+
+from discord import Intents, Object
+from discord.ext.commands import Bot
+
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv, find_dotenv
+
+class ChannelSyncBot(Bot):
+	def __init__(self):
+		intents = Intents.all()
+		super().__init__(intents = intents, command_prefix = "/")
+
+	async def start(self, *args, **kwargs):
+		self.core_guild = int(os.getenv("GUILD"))
+		await super().start(*args, **kwargs)
+
+	async def sync_commands(self):
+		await self.tree.sync(guild = Object(id = self.core_guild))
+		await self.tree.sync()
+
+	async def setup_hook(self) -> Coroutine[Any, Any, None]:
+		if os.getenv("MONGO_TLS") == "True":
+			self.database = AsyncIOMotorClient(
+				os.getenv("MONGO"),
+				tls = True,
+				tlsCertificateKeyFile = "mongo_cert.pem"
+			)["channelsync"]
+		else:
+			self.database = AsyncIOMotorClient(os.getenv("MONGO"))["channelsync"]
+
+		for file in os.listdir("./cogs"):
+			if file.endswith(".py"):
+				try:
+					await self.load_extension(f"cogs.{file[:-3]}")
+					print(f"Loaded \"{file[:-3].capitalize()}\" extension")
+				except Exception as error:
+					traceback.print_exc(error)
+
+		self.loop.create_task(self.sync_commands())
+
+if __name__ == "__main__":
+	load_dotenv(find_dotenv())
+	bot = ChannelSyncBot()
+	bot.run(os.getenv("TOKEN"))
