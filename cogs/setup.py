9b11b69503aba36b0f55d389b7ef402d6af797a4
Snyco <hello@snyco.dev>
2024-07-19 18:40:25 +0800

    Removed `Argument` decorator due to errors

--- cogs/setup.py
+++ cogs/setup.py
@@ -1,5 +1,5 @@
-from discord import Argument, Choice, Interaction
-from discord.app_commands import command, describe
+from discord import Interaction
+from discord.app_commands import command
 from discord.ext.commands import Bot, Cog
 
 class Setup(Cog):
@@ -7,27 +7,13 @@ class Setup(Cog):
 		self.bot = bot
 
 	@command(name = "link", description = "Link global chat to a channel.")
-	@Argument(
-		name = "filter",
-		description = "Enable or Disable profanity filter.",
-		choices = [
-			Choice(
-				name = "Enable",
-				value = True
-			),
-			Choice(
-				name = "Disable",
-				value = False
-			)
-		]
-	)
 	async def link(self, interaction: Interaction, filter: bool):
 		if len(self.bot.database["config"].find({"_id": interaction.guild.id})) == 0:
 			webhook_url = await interaction.channel.create_webhook(
 				name = self.bot.user.name,
 				avatar = self.bot.user.avatar,
 				reason = "Linking channel to global chat."
-			).
+			).url
 			await self.bot.database["config"].insert_one(
 				{
 					"_id": interaction.guild.id,
